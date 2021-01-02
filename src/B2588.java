52ddd3a5175cfcbe0b3c32144997486923019ac6
kwr0113 <kwr0113@gmail.com>
2021-01-02 19:05:41 +0900

    1. 입출력과 사칙연산(3) + 2. If문(5) + 3. for문(1)

--- src/B2588.java
+++ src/B2588.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class B2588 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
---
+++ src/B2884.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class B2884 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int time = h * 60 + m - 45;
+		
+		if ( time >= 0 ) {
+			System.out.println( (time/60) + " " + (time%60) );
+		} else if ( time < 0 ) {
+			System.out.println("23 " + (15+m) );
+		}
+
+	}
+
+}
