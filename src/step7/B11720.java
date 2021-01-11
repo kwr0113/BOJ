6e87d900af1f7ec4a010b53681344916e17b4eba
kwr0113 <kwr0113@gmail.com>
2021-01-11 21:15:27 +0900

    6. 함수 (1) + 7. 문자열 (5)

---
+++ src/step7/B11720.java
@@ -0,0 +1,20 @@
+package step7;
+
+import java.util.Scanner;
+
+public class B11720 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		String para = sc.next();
+		int total = 0;
+
+		for(int i = 0; i < num; i++) {
+			total += Character.getNumericValue(para.charAt(i));		
+		}
+		
+		System.out.println(total);
+	}
+}
---
+++ src/step7/B2675.java
@@ -0,0 +1,30 @@
+package step7;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B2675 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int num = Integer.parseInt(br.readLine()); // 테스트케이스 개수
+		
+		for(int i = 0; i < num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken()); // 반복할 횟수 r
+			String s = st.nextToken(); // 반복할 문자 s
+
+			for(int j = 0; j < s.length(); j++) { // s의 길이만큼 수행
+				for(int k = 0; k < r; k++) { // s를 하나씩 잘라서 r만큼 버퍼에 입력해줌
+					bw.write(s.charAt(j));
+				}
+			}
+			bw.newLine(); // s 입력 후에 개행
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
